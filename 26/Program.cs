770fb2c76cd9a0c2b88ef1bad27e859d6a0fb6d9
fake_state <ntdako@gmail.com>
2023-02-21 00:43:46 +0400

    add 26

---
+++ 26/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите метод, который принимает 
+на вход число и выдаёт сумму цифр в числе.*/
+
+int Number()
+{
+    Console.WriteLine("Введи число и я покажу сумму его цифр:");
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+int Sum(int n)
+{
+    int sum = 0;
+    int temporary = n;
+    while(n > 10)
+    {
+       temporary = n % 10;
+       sum = sum + temporary;
+       n = n / 10;
+    }
+    sum = sum + n;
+    return sum;
+}
+
+int N = Number();
+
+Console.WriteLine(Sum(N));
