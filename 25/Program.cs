8508e462a006592de0357bb68d96034403f4c4df
fake_state <ntdako@gmail.com>
2023-02-20 14:15:17 +0400

    add 25

---
+++ 25/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите метод, который принимает
+ на вход два числа (A и B) и возводит число A в натуральную степень B.
+Math.Pow использовать нельзя*/
+
+int Number() // Ввод числа
+{
+    int N = Convert.ToInt32(Console.ReadLine());
+    return N;
+}
+
+int Cube(int A, int B)
+{
+    int cube = A;
+    for (int i = 1; i <= B; i++)
+    {
+        cube = cube*A;
+    }
+    return cube;
+}
+
+Console.WriteLine("Введите число, которое нужно возвести в степень:");
+int A = Number();
+Console.WriteLine("В какую степень нужно возвести:");
+int B = Number();
+
+int cube = Cube(A, B);
+Console.WriteLine($"{A}^{B} = {cube}");
