aa7f0da84d21e7ed5ed12d1e090a79a3bdef6fae
fake_state <ntdako@gmail.com>
2023-03-01 15:23:27 +0400

    add count

--- dop1/Program.cs
+++ dop1/Program.cs
@@ -20,16 +20,53 @@ void FillArray(int[] x, int N)
 
 void PrintArray(int[] x)
 {
-    int index = 0;
-    int length = x.Length;
     Console.WriteLine("Ваш массив: ");
-    for ( index = 0; index < length; index++)
+
+    for ( int index = 0; index < x.Length; index++)
     {
         Console.Write($"{x[index]} ");
     }
+    Console.WriteLine(" ");
+    Console.WriteLine(" ");
 }
+
+void Count(int[] array)
+{
+    int indexOutside = 0;
+    int number = 0;
+    int count = 0;
+    int indexInside = 0;
+    for (indexOutside = 0; indexOutside < array.Length; indexOutside++)
+    {
+        number = array[indexOutside];
+        for (indexInside = 0; indexInside < array.Length; indexInside++)
+        {
+            if (array[indexInside] == number)
+                count++;
+        }
+    
+        Console.WriteLine(number + " repit " + count + " times");
+        array = array.Distinct().ToArray();
+        
+        indexInside = 0;
+        count = 0;
+    }
+}
+// void Count(int[] x, int N)
+// {
+//     int[] count = new int[N];
+//     foreach (int number in x)
+//     {
+//         count[number - 1]++;
+//     }
+//     Console.WriteLine(string.Join(" ", x));
+//     Console.WriteLine(string.Join(" ", count));
+    
+// }
+
 int N = NumberFillArray("Введите число");
 int[] Array = new int[N];
 FillArray(Array, N);
 PrintArray(Array);
+Count(Array);
 
