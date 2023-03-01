917759a5254f68c30cebb2c1b94b5da1bfa4ab2a
fake_state <ntdako@gmail.com>
2023-03-01 13:47:25 +0400

    add dop1 fill and print array without count

---
+++ dop1/Program.cs
@@ -0,0 +1,35 @@
+﻿// дан массив: наполнени числами от 1 до N
+// Задача:выяснить сколько раз какое число встречается
+
+int NumberFillArray(string text)
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void FillArray(int[] x, int N)
+{
+    int index = 0;
+    int length = x.Length;
+    for (index = 0; index < length; index++)
+    {
+        x[index] = new Random().Next(1, N);
+    }
+}
+
+void PrintArray(int[] x)
+{
+    int index = 0;
+    int length = x.Length;
+    Console.WriteLine("Ваш массив: ");
+    for ( index = 0; index < length; index++)
+    {
+        Console.Write($"{x[index]} ");
+    }
+}
+int N = NumberFillArray("Введите число");
+int[] Array = new int[N];
+FillArray(Array, N);
+PrintArray(Array);
+
